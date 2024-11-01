01459fdf5df5bcde3791b48900e89f0c2eb58de4
ZennithYattare <luis.espera.0758@gmail.com>
2024-11-02 00:50:38 +0800

    test: simulate broken deployment

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.use(express.static('dist'))
 
 // NOTE: this is a simple health check endpoint
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
 
